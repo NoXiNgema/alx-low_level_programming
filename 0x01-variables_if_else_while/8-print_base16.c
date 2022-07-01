89a1dda15cc467a0a50d594ac626d9b50cfa9f47
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-01 09:51:29 -0700

    prints all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints reversed alphabet
+ * main - Prints all the numbers of base 16 in lowercase
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i > 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
