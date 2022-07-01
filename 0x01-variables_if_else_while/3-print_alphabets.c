8bb6f74dd524aed944dc1cc11f5d54202ddc759f
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-01 08:41:32 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the Alphabet in lowercase and in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		int r;
+
+	if (r = 0;r < 52; r++)
+	{
+		putchar(ch[r]);
+	}
+	putchar('\n');
+	return (0);
+}
