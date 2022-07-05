d28a5c4ffdd19e4214558af1f99560f898c93675
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-05 12:28:11 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int position, size;
+
+	size = sizeof(str) / sizeof(int);
+	for (position = 0; position < size; position++)
+	{
+		_putchar(str[position]);
+	}
+	_putchar("\n");
+	return (0);
+}
