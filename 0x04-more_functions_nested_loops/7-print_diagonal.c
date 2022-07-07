28e1867f6d68c51db2c8e93ed3416eeb9e38abdc
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-07 15:41:10 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,12 @@ void print_diagonal(int n)
 	{
 		int i;
 		int j = 32;
-		int k;
 
 		for (i = 0; i < n; i++)
 		{
-			for (k = i; k < n; k++)
-			{
-				_putchar(j) * k;
-				_putchar(92);
-				_putchar('\n');
-			}
+			_putchar(j) * i;
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 	else if (n <= 0)
