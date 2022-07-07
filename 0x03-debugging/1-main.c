a116fffb515a846b1f6c3edca82cacd9de657a62
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-07 16:54:55 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - Removing infinite loop.
+ *main - causes an infinite loop
  *Return: 0
  */
 
@@ -10,6 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 
 	/*while (i < 10)*/
