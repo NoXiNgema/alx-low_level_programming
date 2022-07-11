151eabdeba3b7ccbe651587fe9dbf9ab2ff7b9af
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-11 14:22:09 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: Pointer to integer
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
