71462de40e0357bd81a6693950dd43bab7219434
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-11 13:44:47 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates the
  *  value it points to to 98
- *
+ *  @n: Pointer to a value
  *  Return: no return
  */
 void reset_to_98(int *n);
