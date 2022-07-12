53056ef30aa96e244a7947f5b2652d0110e53d67
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-07-11 18:08:29 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,8 +20,12 @@ void puts_half(char *str)
 	if (count % 2 == 1)
 		i = count / 2;
 	else
+	{
 		i = (count - 1) / 2;
+	}
 		for (i++; i < count; i++)
+		{
 			_putchar(str[i]);
+		}
 		_putchar('\n');
 }
