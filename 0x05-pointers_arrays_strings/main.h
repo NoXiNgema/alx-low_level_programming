b3e212f7bf480e88462485b8b9f4f58061485283
NoXiNgema <107004410+NoXiNgema@users.noreply.github.com>
2022-07-12 02:54:33 +0200

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,16 +8,3 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-[master 06f18f8] vi 7-puts_half.c
- 2 files changed, 28 insertions(+)
- create mode 100644 0x05-pointers_arrays_strings/7-puts_half.c
-On branch master
-Your branch is ahead of 'origin/master' by 1 commit.
-  (use "git push" to publish your local commits)
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   main.h
-
-no changes added to commit (use "git add" and/or "git commit -a")
