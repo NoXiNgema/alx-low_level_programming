e49f649164ef24bac5c9089fd361076ccdfd86ae
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-08-01 12:46:17 -0700

    Define a new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * Define a new type struct dog with the following elements
+ * name, type = char *
+ * age, type = float
+ * owner, type = char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner'
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Pointers, arrays and strings
 0x0D. C - Preprocessor
+ Structures, typedef
