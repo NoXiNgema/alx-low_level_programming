87ef59ab8084ff8cadb242f37a50b2909756e432
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-08-03 14:01:15 -0700

    function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: Given name
+ * @f:Function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
