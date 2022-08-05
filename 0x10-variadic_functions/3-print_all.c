a561e16962e968ebc33df5c8d8b7ca18ce39ca66
Nokuphiwa Ngema <ngemanokuphiwa@gmail.com>
2022-08-05 04:25:38 -0700

    check

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include <stderg.h>
+#include <stdio.h>
+#include <variadic_functions.h>
+
+/**
+ * print_all - prints anything
+ * @format: is a list of types of arguments passed to the function
+ * Return: no return
+ */
+
+void print_all(const char * const format, ...)
+	
